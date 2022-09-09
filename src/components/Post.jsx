ce9cfd83ede79f874d826f519f82d06041980d34
deividsonsabino <deividsonsabino@outlook.com.br>
2022-09-08 23:54:22 -0300

    feat(post): add post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import styles from './Post.module.css'
+
+export function Post(props) {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/33181693?v=4" />
+                    <div className={styles.authorInfo}>
+                        <strong>{props.author}</strong>
+                        <p>{props.content}</p>
+                    </div>
+                </div>
+
+                <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+                <p>👉 <a href="#">jane.design/doctorcare </a></p>
+
+                <p><a href="#">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article >
+    )
+}
